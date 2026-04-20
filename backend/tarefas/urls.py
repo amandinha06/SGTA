116fac7a857e084be97a693de7be5dd8a261e391
amandinha06 <a.dinjz06@gmail.com>
2026-04-19 21:32:12 -0300

    mudancas_dever_2

--- backend/tarefas/urls.py
+++ backend/tarefas/urls.py
@@ -1,10 +1,22 @@
 from django.urls import path
 from .views import listar_tarefas
 from .views import listar_tarefas_abertas
+from .views import tarefas_urgentes
+from .views import tarefas_nao_urgente
+from .views import buscar_id
+from .views import busca_dupla
+from .views import tarefas_atrasadas
+from .views import busca_palavra
 
 urlpatterns = [
     path('tarefas/', listar_tarefas),
-    path('abertas/', listar_tarefas_abertas)
+    path('abertas/', listar_tarefas_abertas),
+    path('urgentes/', tarefas_urgentes),
+    path('naourgentes/', tarefas_nao_urgente),
+    path('tarefas/<int:id>/', buscar_id),
+    path('abertasugentes/', busca_dupla),
+    path('atrasadas/', tarefas_atrasadas),
+    path('tarefas/<str:palavra>/', busca_palavra)
 ]
 
 # rotas
--- backend/tarefas/views.py
+++ backend/tarefas/views.py
@@ -2,6 +2,7 @@
 # primeiro get 
 from django.http import JsonResponse
 from .models import Tarefa
+from datetime import date
 
 def listar_tarefas(request):
     tarefas = Tarefa.objects.all().values()
@@ -10,3 +11,28 @@ def listar_tarefas(request):
 def listar_tarefas_abertas(request):
     tarefas = Tarefa.objects.filter(status='ABERTA').values()
     return JsonResponse(list(tarefas), safe=False)
+
+def tarefas_urgentes(request):
+    tarefas = Tarefa.objects.filter(prioridade='URGENTE').values()
+    return JsonResponse(list(tarefas), safe=False)
+
+def tarefas_nao_urgente(request):
+    tarefas = Tarefa.objects.filter(prioridade='NAO_URGENTE').values()
+    return JsonResponse(list(tarefas), safe=False)
+    
+def buscar_id(request, id):
+    tarefa = Tarefa.objects.filter(id=id).values()
+    return JsonResponse(list(tarefa), safe=False)
+      
+def busca_dupla(request):
+    tarefas = Tarefa.objects.filter(status='ABERTA',prioridade='URGENTE' ).values() 
+    return JsonResponse(list(tarefas), safe=False)
+
+def tarefas_atrasadas(request):
+    hoje = date.today()
+    tarefas = Tarefa.objects.filter(data_entrega__lt=hoje).values()
+    return JsonResponse(list(tarefas), safe=False)
+
+def busca_palavra(request, palavra):
+    tarefa = Tarefa.objects.filter(titulo__icontains=palavra).values()
+    return JsonResponse(list(tarefa), safe=False)
